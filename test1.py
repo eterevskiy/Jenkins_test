64eaed22088b99fc83d6fa17b2f184b4b6dea917
eterevskiy <eterevskiy.viktor@gmail.com>
2023-04-14 00:12:36 +0300

    Commit

--- test1.py
+++ test1.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 import os 
 import time
-driver = webdriver.Chrome(executable_path=os.path.join(os.getcwd(),'chromedriver'))
+chrome_path = '/usr/bin/google-chrome'
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+service = Service(executable_path=os.path.join(os.getcwd(),'chromedriver'))
+driver = webdriver.Chrome(service=service, options=chrome_options,executable_path=chrome_path)
 try :
     driver.get('https://www.google.com/')
     time.sleep(3)
