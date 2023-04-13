2f8f44ed079f086653b17bc54ae3c36fbf19fee1
eterevskiy <eterevskiy.viktor@gmail.com>
2023-04-13 15:12:41 +0300

    commit

--- test1.py
+++ test1.py
@@ -1,1 +1,9 @@
-print('It works')
+from selenium import webdriver
+import time
+driver = webdriver.Chrome(executable_path="chromedriver")
+try :
+    driver.get('https://www.google.com/')
+    time.sleep(3)
+    driver.get_screenshot_as_file('result.png')
+except Exception as err:
+    print(err)
