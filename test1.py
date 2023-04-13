5fe4b12af869376dd25960ad7b042f9b5326f32b
eterevskiy <64526979+eterevskiy@users.noreply.github.com>
2023-04-13 21:32:59 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,11 +1,11 @@
-# from selenium import webdriver
-# import time
-# driver = webdriver.Chrome(executable_path="chromedriver")
-# try :
-#     driver.get('https://www.google.com/')
-#     time.sleep(3)
-#     driver.get_screenshot_as_file('result.png')
-# except Exception as err:
-#     print(err)
+from selenium import webdriver
+import time
+driver = webdriver.Chrome(executable_path="chromedriver")
+try :
+    driver.get('https://www.google.com/')
+    time.sleep(3)
+    driver.get_screenshot_as_file('result.png')
+except Exception as err:
+    print(err)
 import os
 print(os.curdir)
